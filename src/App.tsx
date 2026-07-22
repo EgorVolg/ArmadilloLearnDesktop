25eb264e25f9cdb0ac50bca9b2fa5bea0325fd1a
Egor Volg <volgegor388@gmail.com>
2026-07-22 20:07:45 +0300

    feat: redesign app UI with custom font and word details layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,76 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
+import flag from "./assets/Flag_of_Russia.png";
+
+const bookmark = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="34px"
+    viewBox="0 -960 960 960"
+    width="34px"
+    fill="#F0A06C"
+  >
+    <path d="M200-120v-640q0-33 23.5-56.5T280-840h400q33 0 56.5 23.5T760-760v640L480-240 200-120Zm80-122 200-86 200 86v-518H280v518Zm0-518h400-400Z" />
+  </svg>
+);
+
+const bookmark_check = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="34px"
+    viewBox="0 -960 960 960"
+    width="34px"
+    fill="#f46200"
+  >
+    <path d="M200-120v-640q0-33 23.5-56.5T280-840h400q33 0 56.5 23.5T760-760v640L480-240 200-120Z" />
+  </svg>
+);
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+  const [check, setCheck] = useState(false); 
 
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+    <div className="App">
+      <button className="bookmark" onClick={() => setCheck(!check)}>
+        {check ? bookmark : bookmark_check}
+      </button>
+      <main className="container">
+        <article>
+          <div className="container-header">
+            <h1>Слово</h1>
+            <hr />
+          </div>
+          <section className="word">Capability</section>
+        </article>
 
-      <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+        <article>
+          <section className="container-header">
+            <h1>Перевод</h1>
+            <hr />
+          </section>
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
+          <section className="tags">
+            <span>Noun</span>
+            <span>Programming</span>
+          </section>
+
+          <section className="word">
+            <img src={flag} alt="russian language" />
+            Возможность
+          </section>
+
+          <section className="definition">
+            The power or ability to do something, or a specific feature or
+            function that a system has The power or ability to do something, or
+            a specific feature or function that a system has
+          </section>
+
+          <section className="synonyms">
+            Synonyms:<span>&nbsp;ability, capacity, functionality</span>
+          </section>
+        </article>
+      </main>
+    </div>
   );
 }
 
